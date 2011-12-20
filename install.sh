d74dd6cc457d70b741045d260bc02b2317546870
jmj <jeremyj@letifer.org>
2011-12-20 10:43:05 -0600

    remove tmp for install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ DIRS=".zsh.d .vim"
 
 scriptdir=$(dirname $(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}"))
 
-DEST=${HOME}/tmp
+DEST=${HOME}
 
 for f in ${FILES}; do
     ln -s ${scriptdir}/${f} ${DEST}
